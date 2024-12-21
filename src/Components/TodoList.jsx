3c5e14bb2e7fea0f173f5eeaaf884e3e5a43b613
= <=>
2024-12-21 15:47:39 -0500

    updated todo component

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -26,10 +26,18 @@ const TodoList = () => {
             value={headingInput}
             onChange={(e) => {setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
         />
-          <button className="add-list-button" >Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
+        {todos.map((todo, index) => (
+        <div key={index} className="todo-card">
+        <div className="heading_todo">
+            <h3>{todo.heading}</h3> {/* Display the heading here */}
+            <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+        </div>
+        </div>
+    ))}
         
       </div>
     </>
